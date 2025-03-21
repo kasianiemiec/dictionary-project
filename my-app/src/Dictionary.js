d44ebe2b72c3bca69cfa1285e66c0a518b39790e
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-03-21 23:07:09 +0200

    implemented dictionary API

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function displayWord(response) {
+    alert(response.data.phonetic);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiKey = "98bc9f158331o422ta5db05dbd90ba36";
+    let word = keyword;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayWord);
   }
 
   function handleKeywordChange(event) {
