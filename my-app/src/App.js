1c2d98e7987f280468bf23cc917db90192ad17f8
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-03-21 15:47:04 +0200

    added search form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hey there from react app</header>
+      <div className="container m-5">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>This app is coded by Kasia Niemiec.</small>
+        </footer>
+      </div>
     </div>
   );
 }
