c12e0a6a8c873a6f476aaecd367f4e728875704d
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-03-24 11:43:44 +0200

    fixed if statement in re to examples

--- my-app/src/Meaning.js
+++ my-app/src/Meaning.js
@@ -3,20 +3,34 @@ import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
   console.log(props.meaning);
-  return (
-    <div className="Meanings">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      <p>
-        <strong>Definition: </strong>
-        {props.meaning.definition}
-      </p>
-      <br />
-      <p>
-        <strong>Example: </strong> <em>{props.meaning.example}</em>
-      </p>
-      <br />
-      <strong>Synonyms: </strong>
-      <Synonyms synonyms={props.meaning.synonyms} />
-    </div>
-  );
+  if (props.meaning.example) {
+    return (
+      <div className="Meanings">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>
+          <strong>Definition: </strong>
+          {props.meaning.definition}
+        </p>
+        <br />
+        <p>
+          <strong>Example: </strong> <em>{props.meaning.example}</em>
+        </p>
+        <br />
+        <strong>Synonyms: </strong>
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Meanings">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>
+          <strong>Definition: </strong>
+          {props.meaning.definition}
+        </p>
+        <strong>Synonyms: </strong>
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
+    );
+  }
 }
